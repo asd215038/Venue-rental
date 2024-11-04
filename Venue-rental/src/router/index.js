2fcf347e7c6df628145b903c181b061d9353ce11
asd215038 <asd215038@gmail.com>
2024-11-04 16:43:45 +0800

    init
    初始化專案 使用 vue router

---
+++ Venue-rental/src/router/index.js
@@ -0,0 +1,83 @@
+import { createStore } from 'vuex'
+import { firebaseAuth } from '@/config/firebaseConfig';
+import { 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword, 
+  signOut,
+  updateProfile 
+} from 'firebase/auth';
+
+export default createStore({
+  state: {
+    user: {
+      loggedIn: false,
+      displayName: null,
+      email: null
+    }
+  },
+  getters: {
+    user(state) {
+      return state.user
+    }
+  },
+  mutations: {
+    SET_LOGGED_IN(state, value) {
+      state.user.loggedIn = value;
+    },
+    SET_USER_DISPLAYNAME(state, displayName) {
+      state.user.displayName = displayName;
+    },
+    SET_USER_EMAIL(state, email) {
+      state.user.email = email;
+    }
+
+  },
+  actions: {
+    async register(context, { email, password, name }) {
+      const response = await createUserWithEmailAndPassword(firebaseAuth, email, password)
+      if (response) {
+        await updateProfile(response.user, {
+          displayName: name
+        })
+        context.commit('SET_USER_DISPLAYNAME', response.user.displayName)
+        context.commit('SET_USER_EMAIL', response.user.email)
+        context.commit('SET_LOGGED_IN', true)
+        console.log(name)
+      } else {
+        throw new Error('Unable to register user')
+      }
+    },
+
+    async logIn(context, { email, password }) {
+      const response = await signInWithEmailAndPassword(firebaseAuth, email, password)
+      if (response) {
+        context.commit('SET_USER_DISPLAYNAME', response.user.displayName)
+        context.commit('SET_USER_EMAIL', response.user.email)
+        context.commit('SET_LOGGED_IN', true)
+        console.log(response.user)
+      } else {
+        throw new Error('login failed')
+      }
+    },
+
+    async logOut(context) {
+      await signOut(firebaseAuth)
+      context.commit('SET_USER_DISPLAYNAME', null)
+      context.commit('SET_USER_EMAIL', null)
+      context.commit('SET_LOGGED_IN', false)
+    },
+    // async fetchUser(context, user) {
+    //   context.commit("SET_LOGGED_IN", user !== null);
+    //   if (user) {
+    //     context.commit("SET_USER", {
+    //       displayName: user.displayName,
+    //       email: user.email
+    //     });
+    //   } else {
+    //     context.commit("SET_USER", null);
+    //   }
+    // }
+  },
+  modules: {
+  }
+})
---
+++ Venue-rental/src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <div v-if="isLoggedIn">
+      <p>使用者名稱: {{ user.displayName }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'HomeView',
+  
+  computed: {
+    user() {
+      console.log(this.$store.state.user)
+      return this.$store.state.user
+    },
+    isLoggedIn() {
+      console.log(this.$store.state.user.loggedIn)
+      return this.$store.state.user.loggedIn
+    }
+  }
+}
+</script>
