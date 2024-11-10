249c7f9a4e0ef926678164b2bf13310ad394d2a8
呂雅慈 <161418704+D1204504@users.noreply.github.com>
2024-11-10 21:19:50 +0800

    修改註冊畫面 新增footer 修改navbar 修正登出功能

--- Venue-rental/src/components/navBar.vue
+++ Venue-rental/src/components/navBar.vue
@@ -1,54 +1,61 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <router-link class="navbar-brand" to="/">Home</router-link>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+  <nav class="navbar navbar-expand-lg navbar-light custom-bg">
+    <router-link class="navbar-brand" to="/">Home</router-link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">場地租借</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">場地查詢</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">訂單查詢</a>
-                </li>
-                <!-- <li v-if="isLoggedIn" class="nav-item"> 設定僅限登入時能夠檢視 -->  
-                <li class="nav-item">
-                    <a class="nav-link" href="/user/manage">後臺管理</a>
-                </li>
-            </ul>
-            <div v-if="isLoggedIn" class="userName">
-                <p>使用者名稱: {{ user.displayName }}</p>
-            </div>
-            <router-link v-if="isLoggedIn" class="btn btn-outline-success my-2 my-sm-0" to="logout">登出</router-link>
-            <router-link v-else class="btn btn-outline-success my-2 my-sm-0" to="/login">登入 / 註冊</router-link>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link class="nav-link" to="#" active-class="font-weight-bold">場地租借</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="#" active-class="font-weight-bold">場地查詢</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="#" active-class="font-weight-bold">訂單查詢</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/user/manage" active-class="font-weight-bold">後臺管理</router-link>
+        </li>
+      </ul>
+
+      <div class="navbar-nav ml-auto">
+        <router-link class="nav-link btn btn-outline-primary mr-2" to="/">首頁</router-link>
+        <div v-if="isLoggedIn" class="userName mr-2">
+          <p>{{ user.displayName }}</p>
         </div>
-    </nav>
+        <router-link v-if="isLoggedIn" class="btn btn-outline-success my-2 my-sm-0" to="logout">登出</router-link>
+        <router-link v-else class="btn btn-outline-success my-2 my-sm-0" to="/login">登入 / 註冊</router-link>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
 export default {
-    computed: {
-        user() {
-            console.log(this.$store.state.user)
-            return this.$store.state.user
-        },
-        isLoggedIn() {
-            console.log(this.$store.state.user.loggedIn)
-            return this.$store.state.user.loggedIn
-        }
+  computed: {
+    user() {
+      return this.$store.state.user;
+    },
+    isLoggedIn() {
+      return this.$store.state.user.loggedIn;
     }
-}
+  }
+};
 </script>
 
 <style>
+.custom-bg {
+  background-color: #39B3E3 !important;
+}
 .userName {
-    display: flex;
-    align-items: center; /* 垂直置中 */
+  display: flex;
+  align-items: center;
+}
+.font-weight-bold {
+  font-weight: bold;
 }
 </style>
