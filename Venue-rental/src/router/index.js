15ce76f74656e975508ce0e393a1f7e831f04710
asd215038 <asd215038@gmail.com>
2024-11-13 14:09:13 +0800

    refactor : 檔案路徑重構 (dir : auth , file : router/index.js)
    auth 原本放在components 是較為不好的 應存放於views 在router做引用時比較有系統性的架構

--- Venue-rental/src/router/index.js
+++ Venue-rental/src/router/index.js
@@ -1,26 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import userLogin from "../components/auth/userLogin.vue"
-import userRegister from "../components/auth/userRegister.vue"
-import userLogout from "../components/auth/userLogout.vue"
+import userLogin from "../views/auth/userLogin.vue"
+import userRegister from "../views/auth/userRegister.vue"
+import userLogout from "../views/auth/userLogout.vue"
 import UserManageVIew from '../views/UserManageVIew.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ '../views/UserManageVIew.vue')
-    },
+    { path: '/', name: 'home', component: HomeView },
     { path: "/login", component: userLogin },
     { path: "/register", component: userRegister },
     { path: "/logout", component: userLogout },
